c9aaa11227c45dfdb448e93b49ba72b41a7838bf
Pritesh <me@pritesh.net>
2022-06-06 15:02:23 +0100

    Added dynamo table viewer

--- src/main/java/com/myorg/JavaStack.java
+++ src/main/java/com/myorg/JavaStack.java
@@ -1,17 +1,44 @@
 package com.myorg;
 
-import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.Stack;
-import software.amazon.awscdk.core.StackProps;
+import io.github.cdklabs.dynamotableviewer.TableViewer;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.constructs.Construct;
 
 public class JavaStack extends Stack {
-    public JavaStack(final Construct scope, final String id) {
-        this(scope, id, null);
+
+    public JavaStack(final Construct parent, final String id) {
+        this(parent, id, null);
     }
 
-    public JavaStack(final Construct scope, final String id, final StackProps props) {
-        super(scope, id, props);
+    public JavaStack(final Construct parent, final String id, final StackProps props)
+    {
+        super(parent, id, props);
 
-        // The code that defines your stack goes here
+        final Function hello = Function.Builder.create(this, "HelloHandler")
+                .runtime(Runtime.NODEJS_14_X)
+                .code(Code.fromAsset("lambda"))
+                .handler("hello.handler")
+                .build();
+
+        // Defines our hitcounter resource
+        final HitCounter helloWithCounter = new HitCounter(this, "HelloHitCounter", HitCounterProps.builder()
+                .downstream(hello)
+                .build());
+
+        // Defines an API Gateway REST API resource backed by our "hello" function
+        LambdaRestApi.Builder.create(this, "Endpoint")
+                .handler(helloWithCounter.getHandler())
+                .build();
+
+        // Defines a viewer for the HitCounts table
+        TableViewer.Builder.create(this, "ViewerHitCount")
+                .title("Hello Hits")
+                .table(helloWithCounter.getTable())
+                .build();
     }
 }
